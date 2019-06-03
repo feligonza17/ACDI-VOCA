33f620ad69cb77bf26d751bb07309ec267666956
Felipe González Arango <feligonza17@hotmail.com>
2019-06-03 18:59:22 -0500

    Modulo Politica ELCA2016

--- build_site.R
+++ build_site.R
@@ -5,7 +5,9 @@
 setwd("C:/Users/felig/ACDI-VOCA")
 
 #render your sweet site. 
-rmarkdown::render_site("elca_exploracion.Rmd")
+rmarkdown::render_site()
 
-#Felipe, cuando haya un error de compilacion en los graficos (que no aparezca la imagen de un grafico en el html) deber hacer en render_site con el archivo que tiene los graficos que no estan apareciendo. Hacerlo individualmente y, seguro ahi se va a arreglar. 
+#Felipe, cuando haya un error de compilacion en los graficos (que no aparezca la imagen de un grafico en el html) deber hacer en render_site con el archivo que tiene los graficos que no estan apareciendo. Hacerlo individualmente y, seguro ahi se va a arreglar
+
+#Si estas teninedo mas problemas (al hacer los documentos individuales), mejor knitear el documento a html y despues hacer los comando en git. Este render_site se tira las cosas. 
   
