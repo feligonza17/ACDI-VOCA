f9c42da0b8e779aadcd33fda79a0855ae99aeae3
Felipe González Arango <feligonza17@hotmail.com>
2019-06-02 21:58:31 -0500

    Creacion base de datos final

--- build_site.R
+++ build_site.R
@@ -5,7 +5,7 @@
 setwd("C:/Users/felig/ACDI-VOCA")
 
 #render your sweet site. 
-rmarkdown::render_site()
+rmarkdown::render_site("elca_exploracion.Rmd")
 
 #Felipe, cuando haya un error de compilacion en los graficos (que no aparezca la imagen de un grafico en el html) deber hacer en render_site con el archivo que tiene los graficos que no estan apareciendo. Hacerlo individualmente y, seguro ahi se va a arreglar. 
   
